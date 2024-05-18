6fb4bb31092b4e214b2d18c6a3114ca4d98c426f
KeaMohlala <keamogetsemhll@gmail.com>
2024-05-18 01:46:20 -0700

    handle \hbnb path

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+script that starts Flask web application and routes it to
+different pages of the hbnb website
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def home_hbnb():
+    """
+    Returns message on home page
+    """
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """
+    Returns /hbnb page
+    """
+    return "HBNB"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
